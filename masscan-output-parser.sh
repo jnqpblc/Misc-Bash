b7ef772962ecbda87e7c82b9022b4c2892120fea
John Cartrett <jnqpblc@gmail.com>
2021-09-17 11:58:38 -0500

    Update masscan-output-parser.sh

--- masscan-output-parser.sh
+++ masscan-output-parser.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# jnqpblc
-
 if [ -z $2 ]; then printf "\nSyntax: $0 <masscan-output.txt> <{format}|summary|ports|csv|xml|sqlite>\n\n"
 else
   if [ $2 == "summary" ]; then
@@ -12,9 +10,10 @@ else
   elif [ $2 == "ports" ]; then
     printf "\nMasscan found $(sort -k 6 $1|wc -l) open TCP ports across $(sort -k 6 $1|awk '{print $6}'|sort -u|wc -l) hosts within scope.\n\n"
     for port in $(awk '{print $4}' $1|sort -u); do
-      printf "$port : "; egrep " $port " $1|awk '{print $6}'|sort -n|tr '\n' ' ';
+      count=$(egrep " $port " $1|awk '{print $6}'|sort -u|wc -l)
+      printf "$count - $port : "; egrep " $port " $1|awk '{print $6}'|sort -n|tr '\n' ' ';
       printf '\n';
-    done | sort -n
+    done | sort -nr
   elif [ $2 == "csv" ]; then
     for ip in $(awk '{print $6}' $1|sort -u); do
       printf "$ip,"; egrep "$(echo $ip|sed 's/\./\\./g;')" $1|awk '{print $4}'|sort -n|tr '\n' ','|sed 's/,$//g;';
